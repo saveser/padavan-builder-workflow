e407b8c70a6dafa08d4da5ad68bdb60a564f48c5
Sergey Ryzhov <32198814+saveser@users.noreply.github.com>
2025-11-27 01:20:07 +0600

    Update pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -1,2 +1,3 @@
 sed -i 's/^SRC_VER.*/SRC_VER = 72.2/' padavan-ng/trunk/user/nfqws/Makefile
-find padavan-ng/trunk/user/nfqws \( -not -name Makefile -or -not -name patches \) -print0 | xargs -0 rm -rf --
+cd padavan-ng/trunk/user/nfqws
+find . -maxdepth 1 \( -not -name Makefile -or -not -name patches \) -print0 | xargs -0 rm -rf --

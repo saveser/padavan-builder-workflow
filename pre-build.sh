f383f2be8471aca2c66515d0e3c6aa6fb81ade54
Sergey Ryzhov <32198814+saveser@users.noreply.github.com>
2025-12-04 14:44:57 +0600

    Update pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -1,8 +1,9 @@
+# Настройка версии прошивки
+sed -i 's/^FIRMWARE_ROOTFS_VER.*/FIRMWARE_ROOTFS_VER=XXX/' padavan-ng/trunk/versions.inc
+sed -i 's/^FIRMWARE_BUILDS_VER.*/FIRMWARE_BUILDS_VER=XXXXX/' padavan-ng/trunk/versions.inc
+
 # Настройка версии Zapret
 sed -i 's/^SRC_VER.*/SRC_VER = 72.3/' padavan-ng/trunk/user/nfqws/Makefile
 cd padavan-ng/trunk/user/nfqws
 find . -maxdepth 1 -not -name Makefile -not -name patches -print0 | xargs -0 rm -rf --
 
-# Настройка версии прошивки
-sed -i 's/^FIRMWARE_ROOTFS_VER.*/FIRMWARE_ROOTFS_VER=3.9L/' padavan-ng/trunk/versions.inc
-sed -i 's/^FIRMWARE_BUILDS_VER.*/FIRMWARE_BUILDS_VER=102-by-Save55/' padavan-ng/trunk/versions.inc

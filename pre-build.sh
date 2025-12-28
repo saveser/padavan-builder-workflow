5502a51a57530f28cc462b4c1d0f2be4d887117a
Sergey Ryzhov <32198814+saveser@users.noreply.github.com>
2025-12-28 19:33:09 +0600

    Update pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -3,7 +3,8 @@ sed -i 's/^FIRMWARE_ROOTFS_VER.*/FIRMWARE_ROOTFS_VER=3.9L/' padavan-ng/trunk/ver
 sed -i 's/^FIRMWARE_BUILDS_VER.*/FIRMWARE_BUILDS_VER=102/' padavan-ng/trunk/versions.inc
 
 # Настройка версии Zapret
-COMMIT_HASH=$(git ls-remote https://github.com/bol-van/zapret HEAD | awk '{print $1}')
-git ls-remote --refs --tags https://github.com/bol-van/zapret | grep "$COMMIT_HASH" | awk '{print $2}' | sed -i 's/^SRC_VER.*/SRC_VER = /refs\/tags\////' padavan-ng/trunk/user/nfqws/Makefile
+COMMIT_HASH=$(git ls-remote https://github.com/bol-van/zapret> HEAD | awk '{print $1}')
+ZAPRET_VER=$(git ls-remote --refs --tags https://github.com/bol-van/zapret | grep "$COMMIT_HASH" | awk '{print $2}' | sed 's/refs\/tags\///')
+sed -i 's/^SRC_VER.*/SRC_VER = '$ZAPRET_VER'/' padavan-ng/trunk/user/nfqws/Makefile
 cd padavan-ng/trunk/user/nfqws
 find . -maxdepth 1 -not -name Makefile -not -name patches -print0 | xargs -0 rm -rf --

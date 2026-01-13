1333cfdf6a863fff99cc749c4b53f5540cd8aeb7
Sergey Ryzhov <32198814+saveser@users.noreply.github.com>
2026-01-13 16:23:57 +0600

    Update pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -2,8 +2,8 @@
 sed -i 's/^FIRMWARE_ROOTFS_VER.*/FIRMWARE_ROOTFS_VER=3.9L/' padavan-ng/trunk/versions.inc
 sed -i 's/^FIRMWARE_BUILDS_VER.*/FIRMWARE_BUILDS_VER=102/' padavan-ng/trunk/versions.inc
 
-# Установка последней версии zapret
-ZAPRET_REPO="https://github.com/bol-van/zapret.git"
+# Установка последней версии zapret2
+ZAPRET_REPO="https://github.com/bol-van/zapret2.git"
 ZAPRET_TAGS=$(git ls-remote --tags "$ZAPRET_REPO" | awk '{print $2}' | sed 's/refs\/tags\///g')
 ZAPRET_VER=$(echo "$ZAPRET_TAGS" | sort -V | tail -n 1 | sed 's/^.//')
 sed -i "s/^SRC_VER.*/SRC_VER = $ZAPRET_VER/g" padavan-ng/trunk/user/nfqws/Makefile
